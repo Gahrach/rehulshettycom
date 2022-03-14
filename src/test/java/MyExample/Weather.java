c6626801f09d2a54afff0d5d721be932a3f73bff
Gahrach <hrachya1988@mail.ru>
2022-03-14 13:57:31 -0700

    WeatherAPI

---
+++ src/test/java/MyExample/Weather.java
@@ -0,0 +1,32 @@
+package MyExample;
+
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.responseSpecification;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasItems;
+
+public class Weather {
+    String baseUrl = "https://yahoo-weather5.p.rapidapi.com";
+
+    @Test
+    public void getWeather() {
+        String endpoint = "/weather?location=Yerevan";
+
+
+        given().baseUri(baseUrl).
+                contentType(ContentType.JSON).
+
+
+                header("X-RapidAPI-Key", "22f9b1c97bmsh9fd60b638c59275p1d5c23jsnbbf3b55f5c2f").
+                // header("X-RapidAPI-Host","yahoo-weather5.p.rapidapi.com").
+
+                        when().get(endpoint).
+                then().log().all().assertThat().body("location.city",equalTo("Yerevan")).
+                body("forecasts[0].day",equalTo("Mon"));
+
+    }
+}
